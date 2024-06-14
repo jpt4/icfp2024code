310698759bc312d8c97be79c04597245ac87354c
jpt4 <jpt4@protonmail.ch>
2024-06-14 18:13:07 +0000

    nocksche and nocko.

---
+++ nocko.rkt
@@ -0,0 +1,186 @@
+;;  nocko.rkt
+;;  2024
+;;  Nock 4K in miniKanren
+;;  Racket v8.12
+
+#lang racket
+
+(require minikanren)
+(require minikanren/numbers)
+
+;"A noun is an atom or a cell.  An atom is a natural number.  A cell
+;is an ordered pair of nouns." - these translate directly into mK:
+
+
+
+(define (noun i)
+  (conde
+   [(atom i)]
+   [(cell i)]))
+
+(define (atom i)
+  (fresh (d)
+	 (== `(nat . ,d) i)
+	 (nat d)))
+
+(define (nat i)
+  (fresh (d)
+	 (conde
+	  [(== '(0) i)]
+	  [(== '(1) i)]
+	  [(== `(0 . ,d) i)
+	   (=/= '(0) d)
+	   (nat d)]
+	  [(== `(1 . ,d) i)
+	   (=/= '(0) d)
+	   (nat d)])))
+   
+(define (cell i)
+  (fresh (a d)
+	 (== `(,a ,d) i)
+	 (noun a)
+	 (noun d)))
+
+;Reduce by the first matching pattern; variables match any noun.
+
+;A sequential pattern match implicitly accumulates conditions as each
+;previous match clause fails. To replicate the behavior of a
+;sequential pattern match in a pure relational language, fully
+;disjunct match criteria must be explicitly encoded. This is
+;straightforward for the supermajority of reduction expressions
+;(redexes), but will be a point of discussion late in the
+;implementation. The directive to "reduce by the first matching
+;pattern" suggests a direct term rewriting approach; this is adopted,
+;though an alternative structure of the program using functions
+;instead of expression tags is presented.
+
+;nock(a)             *a
+
+;The first design decision is whether to build a single (nocko)
+;procedure, or separate the nops (nock operations) into individual
+;relations, and if so, with what grouping.  There is also the choice
+;between translating `nock(a)` as an expression to be evaluated by an
+;interpreter (the `nevalo` design) or considerating `nock` to be the
+;name of the evaluator (the `nocko` design). The latter is chosen.
+
+;We begin with a monolithic `nocko` relation, using pattern matching
+;on quoted expressions to perform the redexes.
+
+(define (nock i o) 
+  (nevalo `(tar ,i) o))
+
+(define (nevalo i o)
+  (fresh (a b neg)
+	 (noun a) (noun b)
+	 (conde
+	  [(== `(tar (,a ((nat 0) ,b))) i)
+	   (== `(fas (,b ,a)) o)]
+	  #;[(nexp i neg)
+	   (=/= 't neg)]
+	  )))
+
+(define (nexp i o)
+  (fresh (a b)
+;	 (noun a) (noun b)
+	 (conde
+	  [(== `(tar (,a ((nat 0) ,b))) i)
+	   (== 't o)])))   
+a: noun ->
+a: atom -> |_
+a: cell -> 
+[b c]
+  b: noun -> T
+c: atom -> |_
+c: cell -> 
+[b [d e]]
+    d: atom >=12 -> |_
+  d: cell -> [b [[f g] e]] -> T
+d: atom <12 -> T
+d: 0 -> 
+  e: noun -> T
+d: 1 -> 
+  e: noun -> T
+d: 2 ->
+  e: atom -> |_
+e: cell -> T
+d: 3 -> 
+  e: noun -> T
+d: 4 -> 
+  e: nount -> T
+d: 5 -> 
+  e: atom -> |_
+  e: cell -> [b [7 [f g]]] -> T
+e: cell -> T
+d: 6 ->
+  e: atom -> |_
+e: cell -> [b [6 [f g]]]
+  f: noun -> T
+g: atom -> |_
+g: cell -> [b [6 [f [h i]]]]
+  h: noun -> T
+  i: noun -> T
+d: 7 ->
+  e: atom -> |_
+  e: cell -> [b [7 [f g]]] -> T
+d: 8 ->
+  e: atom -> |_
+  e: cell -> [b [8 [f g]]] -> T
+d: 9 ->
+  e: atom -> |_
+  e: cell -> [b [9 [f g]]] -> T
+d: 10 ->
+  e: atom -> |_
+  e: cell -> [b [10 [f g]]]
+    g: noun -> T
+  f: atom -> |_
+     cell -> [b [10 [[h i] g]]]
+    h: noun -> T
+    i: noun -> T
+d: 11 ->
+  e: atom -> |_
+  e: cell -> [b [11 [f g]]]
+    g: noun -> T
+  f: atom -> |_
+     cell -> [b [11 [[h i] g]]]
+    h: noun -> T
+    i: noun -> T
+d: 11 ->
+  e: atom -> |_
+  e: cell -> [b [11 [f g]]] -> T
+
+;the nock operator `*`, meaning "evaluate", is pronouned "tar" (sTAR),
+;which is used as the tag for `*` terms.
+
+
+;[a b c] [a [b c]] 
+
+;nock expressions are right associative. This redex suggests that, in
+;addition to scanning the reduction rules from top to bottom, the nexp
+;must be scanned from right to left. Given a non-fully delimited nexp
+;[a1 a2 a3 ... ak], matching from left to right (comparable to
+;outermost first reduction) to produce [a1 [a2 a3 ... ak]] requires a3
+;... ak to already be understood as encompassed by `c` in the pattern
+;[a b c]. However, if matched from right to left, [a1 ... ak-1 ak] can
+;be parsed as [a1 ... [ak-1 ak]], with `c` matched to ak in terminal
+;position. For both strategies, the recursive cases must be handled,
+;whether of more than three terms at the same depth of the expression,
+;or decomposition of any of terms a, b, or c into terms that must be
+;auto-consed. The intent of a fully right-associated expression is
+;conveyed by this line of the specification, but the mechanism is
+;implementation dependent. If, for example, c is a deeply nested,
+;non-fully right associated expression, e.g. [0 0 0 0], then there
+;must be a way to focus on c, and perform an internal pattern match.
+
+
+;autocons cannot match a term with four nouns, because it's pattern
+;does not admit a list (of potentially arbitary size >=3), with the
+;tail captured in the pattern variable `c`, but strictly an ordered
+;tuple of three elements. Thus, an unparsed nock([a 6 b c d e]) -> *[a
+;6 b c d e] -> *a -> LOOP. For autocons to group `d` and `e` within
+;the cell `f`, the autocons window must intercept the six element
+;tuple and check it from right to left, grouping as necessary.
+
+;Kelvin versioning is an example of a finite critical developmental
+;period in the lifecycle of a piece of software, with some precedent
+;in Knuth's Pi versioning of TeX, which will similarly close its canon
+;upon his death.
---
+++ nocksche.rkt
@@ -0,0 +1,127 @@
+;;  nocksche.rkt
+;;  2024
+;;  ICFP
+;;  Nock 4K in Racket
+;;  Racket v8.12
+
+#lang racket
+(require racket/match)
+
+;external syntax of nouns is strictly atoms and cells 
+
+;internal syntax includes nock prefix operators 
+
+;`tar` as tag in pattern matching within monolithic nock evaluator,
+;with explicit internal syntax
+
+;(tar) as function applied to nouns, without explicit internal syntax
+
+(define (noun n)              ; A noun
+  (or (atom n) (cell n)))     ; is an atom or a cell.
+
+(define (atom a)              ; An atom
+  (natural? a))               ; is a natural number.
+
+(define (cell c)              ; A cell
+  (match c
+    [`(,a ,b)                 ; is an ordered pair
+     (and (noun a) (noun b))] ; of nouns.
+    [_ #f]))
+
+;This analysis aims to interrogate the claims of the Urbit project
+;regarding the simplicity and naturality of the Nock specification, by
+;examining an implementation of Nock 4K in Racket Scheme and
+;miniKanren, and the process of producing this implementation from the
+;specification.
+
+;This paper is a study in the distance between the specification of
+;Nock, and a compliant specification. Nock is defined by an official,
+;but not formal, pseudocode, suggestive of a series of reduction
+;rules, and framed by natural language scaffolding. Most load bearing
+;of these is:
+
+;Reduce by the first matching pattern; variables match any noun.
+
+;We name this the "evaluation rule", because akin to the suggestivity
+;of the pseudocode, it suggests an evaluation strategy, without fully
+;explicating any in particular.
+
+;Narrow adherence to the evaluation rule will quickly reveal the Nock
+;spec to be insufficient unto itself, because there are right column
+;patterns without left column matches, for which reduction should not
+;yet terminate. E.g., for *[a [b c] d]=>[*[a b c] *[a d]], no left
+;column pattern corresponds to a cell with two tar-prefixed
+;nouns. Therefore, implementation of the evaluation rule must
+;recognize that reduction recurses within the cell, and address each
+;tar expression individually. No longer bound by a narrow adherence to
+;the evaluation rule, the whole field of evaluation strategies would
+;now appear to be available, and the choice of strategy entirely
+;implementation dependent. For the majority of reduction rules, this
+;is indeed the case, but certain reductions will provide, implicitly,
+;additional information beyond the evaluation rul, and thus impose
+;additional constraints on a compliant implementation.
+
+;produce a right associated noun
+
+;Why should we include this normalization step in our implementation?
+;If expressions are passed to `nock` Why should we perform this
+;normalization step? None of the left column patterns are in fully
+;right associated form, so it would seem that any application of `ras`
+;would prematurely malform the expression, leading to
+;loop/termination. As with the evaluation rule, the right association
+;normalization is intended to recur, and so its implementation must
+;make the choice of how to do so. nocksche chooses to convert all
+;input expressions into fully right-associated normal form, and fully
+;parenthesize all reduction rules.
+
+(define (ras a)
+  (match a
+   [(? noun) a]
+   [`(,x ,y . ,z)
+    #:when (not (null? z))
+    `[,(ras (car a)) ,(ras (cdr a))]
+    ]
+   [`(,x ,y . ,z)
+    #:when (null? z)
+    `[,(ras (car a)) . ,(cdr a)]
+    ]
+   ))
+
+'[[1 [[2 3 4 5] 6] 7] [8 9] [10 11]]   
+
+
+(define-syntax test
+  (syntax-rules ()
+    ((_ title tested-expression expected-result)
+     (begin
+       (printf "Testing ~s\n" title)
+       (let* ((expected expected-result)
+              (produced tested-expression))
+         (or (equal? expected produced)
+             (printf "Failed: ~a~%Expected: ~a~%Computed: ~a~%"
+                     'tested-expression expected produced)))))))
+
+(define (run-tests)
+  (begin
+    (test 'valid-noun-atom (noun 5) #t)
+    (test 'valid-noun-cell (noun '[0 0]) #t)
+
+    (test 'invalid-noun-atom (noun 'a) #f)
+    (test 'invalid-noun-cell (noun '[0]) #f)
+
+    (test 'valid-atom (atom 5) #t)
+
+    (test 'invalid-atom-symbol (atom 'a) #f)
+    (test 'invalid-atom-negative (atom -1) #f)
+    (test 'invalid-atom-cell (atom '[0 0]) #f)
+
+    (test 'valid-cell (cell '[5 5]) #t)
+    (test 'valid-cell-nested-head (cell '[[5 4] 3]) #t)
+    (test 'valid-cell-nested-tail (cell '[5 [4 3]]) #t)
+    (test 'valid-cell-nested-head-and-tail (cell '[[5 4] [3 2]]) #t)
+
+    (test 'invalid-cell-long (cell '[5 5 5]) #f)
+    (test 'invalid-cell-short (cell '[5]) #f)
+
+    
+    )) 
